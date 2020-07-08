954a0d764480a80ea37c46d56e6a85caaef0bb4a
lukaszspyra <lukasz.spyrka@gmail.com>
2020-07-08 12:05:40 +0200

    Create test method for numbers 12 and 6

---
+++ src/test/java/puzzles/numbers/AlgorithmsTest.java
@@ -0,0 +1,20 @@
+package puzzles.numbers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AlgorithmsTest {
+
+    @Test
+    void findBiggestDivisorFor12and6() {
+        //given
+        Algorithms algorithms = new Algorithms();
+
+        //when
+        int biggestDivisor = algorithms.findBiggestDivisor(12, 6);
+
+        //then
+        assertEquals(6, biggestDivisor);
+    }
+}
