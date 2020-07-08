4c070ef5390a5d6386ee77e5a1dbc215c95bad35
lukaszspyra <lukasz.spyrka@gmail.com>
2020-07-08 12:21:28 +0200

    Implement method working for 192 and 348 - test green

--- src/main/java/puzzles/numbers/Algorithms.java
+++ src/main/java/puzzles/numbers/Algorithms.java
@@ -3,32 +3,32 @@ package puzzles.numbers;
 public class Algorithms {
 
 
-    /** Finds biggest common int divisor of two natural numbers excluding 0, using Euclidean algorithm - version with '-' operator
-     *
+    /**
+     * Finds biggest common int divisor of two natural numbers excluding 0, using Euclidean algorithm - version with '-' operator
+     * <p>
      * Creats
+     *
      * @param number1 as int
      * @param number2 as int
-     *
      */
 
-    public int findBiggestDivisor(int number1, int number2){
+    public int findBiggestDivisor(int number1, int number2) {
 
         int difference;
 
-        if (number1 > number2){
+        if (number1 > number2) {
 
             difference = number1 - number2;
+            return findBiggestDivisor(difference, number2);
 
-        } else if(number1 == number2) {
+        } else if (number1 == number2) {
 
             return number1;
 
         } else {
             difference = number2 - number1;
+            return findBiggestDivisor(difference, number1);
         }
-
-
-        return difference;
     }
 
 
