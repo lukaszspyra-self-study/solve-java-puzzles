842136e737fc23c49aa233f4960f58dde74392ad
lukaszspyra <lukasz.spyrka@gmail.com>
2020-07-08 12:27:12 +0200

    Create tests for primes - green

--- src/test/java/puzzles/numbers/AlgorithmsTest.java
+++ src/test/java/puzzles/numbers/AlgorithmsTest.java
@@ -29,4 +29,40 @@ class AlgorithmsTest {
         //then
         assertEquals(12, biggestDivisor);
     }
+
+    @Test
+    void findBiggestDivisorFor77and55() {
+        //given
+        Algorithms algorithms = new Algorithms();
+
+        //when
+        int biggestDivisor = algorithms.findBiggestDivisor(77, 55);
+
+        //then
+        assertEquals(11, biggestDivisor);
+    }
+
+    @Test
+    void findBiggestDivisorForPrimes11and7() {
+        //given
+        Algorithms algorithms = new Algorithms();
+
+        //when
+        int biggestDivisor = algorithms.findBiggestDivisor(11, 7);
+
+        //then
+        assertEquals(1, biggestDivisor);
+    }
+
+    @Test
+    void findBiggestDivisorForPrimes12329and22039() {
+        //given
+        Algorithms algorithms = new Algorithms();
+
+        //when
+        int biggestDivisor = algorithms.findBiggestDivisor(12329, 22039);
+
+        //then
+        assertEquals(1, biggestDivisor);
+    }
 }
