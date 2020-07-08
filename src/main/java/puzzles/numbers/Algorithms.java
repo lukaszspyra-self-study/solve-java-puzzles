d311701d39b60ce8001f91a091f3f422d9ec978e
lukaszspyra <lukasz.spyrka@gmail.com>
2020-07-08 12:01:38 +0200

    Create class to implement methods solving numbering algorithms puzzles

---
+++ src/main/java/puzzles/numbers/Algorithms.java
@@ -0,0 +1,20 @@
+package puzzles.numbers;
+
+public class Algorithms {
+
+
+    /** Finds biggest common int divisor of two natural numbers, using Euclidean algorithm - version with '-' operator
+     *
+     * Creats
+     * @param number1 as int
+     * @param number2 as int
+     *
+     */
+
+    public int findBiggestDivisor(int number1, int number2){
+
+        return 0;
+    }
+
+
+}
