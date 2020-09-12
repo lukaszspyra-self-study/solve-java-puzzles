6b8cb881695e58bfc485aa4556fa1226c64c58ea
lukaszspyra <lukasz.spyrka@gmail.com>
2020-09-12 05:18:20 +0200

    Add hierarchy to Rank

--- src/main/java/puzzles/cardsdeck/Rank.java
+++ src/main/java/puzzles/cardsdeck/Rank.java
@@ -2,18 +2,23 @@ package puzzles.cardsdeck;
 
 public enum Rank {
 
-    DEUCE,
-    THREE,
-    FOUR,
-    FIVE,
-    SIX,
-    SEVEN,
-    EIGHT,
-    NINE,
-    TEN,
-    JACK,
-    QUEEN,
-    KING,
-    ACE
+    DEUCE(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    TEN(10),
+    JACK(11),
+    QUEEN(12),
+    KING(13),
+    ACE(14);
 
+    private final int hierarchy;
+
+    Rank(int hierarchy) {
+        this.hierarchy = hierarchy;
+    }
 }
