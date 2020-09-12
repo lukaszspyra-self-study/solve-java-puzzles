2e7c4c5ae56edad39812e6d0390ae50cc9e78c2e
lukaszspyra <lukasz.spyrka@gmail.com>
2020-09-12 03:11:35 +0200

    Add tests for modification of object by calling method on reference variable

--- src/main/java/puzzles/main/RunYourCode.java
+++ src/main/java/puzzles/main/RunYourCode.java
@@ -1,5 +1,7 @@
 package puzzles.main;
 
+import java.util.Arrays;
+
 /**
  * Hello world!
  */
@@ -9,9 +11,20 @@ public class RunYourCode {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
+        Integer[] test1 = new Integer[]{1, 2, 3, 4};
+        Integer[] test2 = new Integer[]{1, 2, 3, 4};
+        triple(test1);
+
+        System.out.println(Arrays.toString(test1));
+
     }
 
+    static void triple(Integer[] integers){
 
+        integers[1] = 2*integers[1];
+        integers = new Integer[]{1};
+        integers[0] = 2* integers[0];
+    }
 
 
 
